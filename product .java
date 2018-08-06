fe7c70bb064fab5a441a56c89ed1a40cdef50100
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-08-06 10:51:07 +0530

    Create product .java

---
+++ product .java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+
+class product
+{  
+    public static void main(String args[])
+    {
+        int x,y;
+        //given x+y=25;x-y=13
+        x=(38/2);
+        System.out.println(x);
+        y=25-x;
+        System.out.println(y);
+        //To find producct od these two numbers
+        int z=x*y;
+        System.out.println(z);
+    }
+}
+        
